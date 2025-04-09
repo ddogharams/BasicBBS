cfacb8ea3cd14dda282fa12018445f5052e81002
hohyeonlee <ddogharams@gmail.com>
2025-04-10 00:29:11 +0900

    Basic BBS 최초 Commit

---
+++ src/main/java/com/hohyeon/basicbbs/basicbbs/BasicBbsApplication.java
@@ -0,0 +1,13 @@
+package com.hohyeon.basicbbs.basicbbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicBbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicBbsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hohyeon/basicbbs/basicbbs/BasicBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hohyeon.basicbbs.basicbbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicBbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
